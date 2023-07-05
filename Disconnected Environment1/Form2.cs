b63d7ddb27a0ebc42cf76f926b79f0d278883fcd
Ammar Fatwa <ammar.fatwa.ft21@mail.umy.ac.id>
2023-07-05 10:09:54 +0700

    test

---
+++ Disconnected Environment1/Form2.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace Disconnected_Environment1
+{
+    public partial class Form2 : Form
+    {
+        private string stringConnection = "data sourcce=AMMAR\\MSSQLSERVER01;" + "database=prodi; User ID=sa;Password=12345";
+        private SqlConnection koneksi;
+
+        private  void refreshform()
+        {
+            textBox1.Text = "";
+            textBox1.Enabled = false;
+            button3.Enabled = false;
+            button4.Enabled = false;
+        }
+        public Form2()
+        {
+            InitializeComponent();
+            koneksi = new SqlConnection(stringConnection);
+            refreshform();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            string nmProdi = textBox1.Text;
+
+            if (nmProdi == "")
+            {
+                MessageBox.Show("Masukkan Nama Prodi", "Warning", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+            else
+            {
+                koneksi.Open();
+                koneksi.Open();
+                string str = "insert into dbo.prodi (nama_prodi)" + "values(@nama_prodi)";
+                SqlCommand cmd = new SqlCommand(str, koneksi);
+                cmd.CommandType = CommandType.Text;
+                cmd.Parameters.Add(new SqlParameter("nama_prodi", nmProdi));
+                cmd.ExecuteNonQuery();
+
+                koneksi.Close();
+                MessageBox.Show("Data Berhasil Di Simpan", "Sukses",MessageBoxButtons.OK,MessageBoxIcon.Information);
+                
+                refreshform();
+            }
+        }
+
+        private void Form2_Load(object sender, EventArgs e)
+        {
+           
+
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            Form1 fr = new Form1();
+            fr.Show();
+            this.Hide();
+        }
+
+        
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            refreshform();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            button1.Enabled = false;  
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ Disconnected Environment1/Form3.cs
@@ -0,0 +1,176 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Disconnected_Environment1
+{
+
+    public partial class Form3 : Form
+    {
+        private string stringConnection = "data source=AMMAR\\MSSQLSERVER01;" + "database=prodi;User ID=sa;Password=12345";
+        private SqlConnection koneksi;
+        private string nim, nama, alamat, jk, prodi;
+        private DateTime tgl;
+        BindingSource customersBindingSource = new BindingSource();
+
+        public Form3()
+        {
+            InitializeComponent();
+            koneksi = new SqlConnection(stringConnection);
+            this.bindingNavigator1.BindingSource = this.customersBindingSource;
+            refreshform();
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form3_Load(object sender, EventArgs e)
+        {
+
+        }
+        private void FormDataMahasiswa_Load()
+        {
+            koneksi.Open();
+            SqlDataAdapter dataAdapter1 = new SqlDataAdapter(
+                new SqlCommand(
+                    "Select m.nim, m.nama_mahasiswa, " + "m.alamat, m.jenis_kelamin, m.tgl_lahir, p.nama_prodi From dbo.mahasiswa m " +
+                    "join dbo.prodi p on m.id_prodi = p.id_prodi",
+                    koneksi
+                )
+            );
+            DataSet ds = new DataSet();
+            dataAdapter1.Fill(ds);
+
+            this.customersBindingSource.DataSource = ds.Tables[0];
+            this.textBox1.DataBindings.Add(
+                new Binding("Text", this.customersBindingSource, "NIM", true));
+            this.textBox2.DataBindings.Add(
+                new Binding("Text", this.customersBindingSource, "nama_mahasiswa", true));
+            this.textBox3.DataBindings.Add(
+                new Binding("Text", this.customersBindingSource, "alamat", true));
+            this.comboBox1.DataBindings.Add(new Binding("Text", this.customersBindingSource, "jenis_kelamin", true));
+            this.dateTimePicker1.DataBindings.Add(new Binding("Text", this.customersBindingSource, "tgl_lahir", true));
+            this.comboBox2.DataBindings.Add(new Binding("Text", this.customersBindingSource, "nama_prodi", true));
+            koneksi.Close();
+        }
+        private void clearBinding()
+        {
+            this.textBox1.DataBindings.Clear();
+            this.textBox2.DataBindings.Clear();
+            this.textBox3.DataBindings.Clear();
+            this.comboBox1.DataBindings.Clear();
+            this.dateTimePicker1.DataBindings.Clear();
+            this.comboBox2.DataBindings.Clear();
+        }
+        private void refreshform()
+        {
+            textBox1.Enabled = false;
+            textBox2.Enabled = false;
+            comboBox1.Enabled = false;
+            textBox3.Enabled = false;
+            dateTimePicker1.Enabled = false;
+            comboBox2.Enabled = false;
+            button1.Enabled = true;
+            button2.Enabled = false;
+            button3.Enabled = false;
+            clearBinding();
+            FormDataMahasiswa_Load();
+        }
+        private void Prodicbx()
+        {
+            koneksi.Open();
+            string str = "select nama_prodi from dbo.prodi";
+            SqlCommand cmd = new SqlCommand(str, koneksi);
+            SqlDataAdapter da = new SqlDataAdapter(str, koneksi);
+            DataSet ds = new DataSet();
+            da.Fill(ds);
+            cmd.ExecuteReader();
+            koneksi.Close();
+            comboBox2.DisplayMember = "nama_prodi";
+            comboBox2.ValueMember = "id_prodi";
+            comboBox2.DataSource = ds.Tables[0];
+        }
+
+        private void Form3_Load_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void buttonAdd_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "";
+            textBox2.Text = "";
+            textBox3.Text = "";
+            dateTimePicker1.Value = DateTime.Today;
+            textBox1.Enabled = true;
+            textBox2.Enabled = true;
+            comboBox1.Enabled = true;
+            textBox3.Enabled = true;
+            dateTimePicker1.Enabled = true;
+            comboBox2.Enabled = true;
+            Prodicbx();
+            button2.Enabled = true;
+            button3.Enabled = true;
+            button1.Enabled = false;
+        }
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            nim = textBox1.Text;
+            nama = textBox2.Text;
+            jk = comboBox1.Text;
+            alamat = textBox3.Text;
+            tgl = dateTimePicker1.Value;
+            prodi = comboBox2.Text;
+            int hs = 0;
+            koneksi.Open();
+            string strs = "select id_prodi from dbo.prodi where nama_prodi = @dd";
+            SqlCommand cm = new SqlCommand(strs, koneksi);
+            cm.CommandType = CommandType.Text;
+            cm.Parameters.Add(new SqlParameter("@dd", prodi));
+            SqlDataReader dr = cm.ExecuteReader();
+
+            while (dr.Read())
+            {
+                hs = int.Parse(dr["id_prodi"].ToString());
+            }
+            dr.Close();
+
+            string str = "insert into dbo.mahasiswa (nim, nama_mahasiswa, jenis_kelamin, alamat, tgl_lahir) " +
+                         "values (@NIM, @Nm, @Jk, @Al, @Tgll)";
+            SqlCommand cmd = new SqlCommand(str, koneksi);
+            cmd.CommandType = CommandType.Text;
+            cmd.Parameters.Add(new SqlParameter("@NIM", nim));
+            cmd.Parameters.Add(new SqlParameter("@Nm", nama));
+            cmd.Parameters.Add(new SqlParameter("@Jk", jk));
+            cmd.Parameters.Add(new SqlParameter("@Al", alamat));
+            cmd.Parameters.Add(new SqlParameter("@Tgll", tgl));
+            cmd.ExecuteNonQuery();
+            koneksi.Close();
+
+            MessageBox.Show("Data Berhasil Disimpan", "Sukses", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            refreshform();
+        }
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            refreshform();
+        }
+
+        private void FormDataMahasiswa_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            Form1 fm = new Form1();
+            fm.Show();
+            this.Hide();
+        }
+    }
+}
